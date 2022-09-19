fe2d446bf294870d11facbefdb0f24c4acd91157
Игорь <bujhtyz228@gmail.com>
2022-09-19 18:02:51 +0300

    Done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', OnSubmit)
+function OnSubmit(evt) {
+    evt.preventDefault()
+    console.dir(evt.currentTarget)
+    const { email, password } = evt.currentTarget.elements;
+    const data = {
+        email: email.value,
+        password: password.value
+    }
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const btnEl = document.querySelector('.change-color') 
+const spanEl = document.querySelector('.color')
+
+btnEl.addEventListener('click', OnClick)
+
+function OnClick() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.replaceChildren(getRandomHexColor())
+}
