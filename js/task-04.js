7222045fae4a3951f654f61f18151b477f84ebb1
Игорь <bujhtyz228@gmail.com>
2022-09-20 11:23:52 +0300

    done

--- js/task-04.js
+++ js/task-04.js
@@ -1,13 +1,16 @@
 let counterValue = (Number = 0);
 
 const clickAdd = document.getElementById("counter");
-const btns = clickAdd.childNodes;
+// const btns = clickAdd.childNodes;
 
 let county = document.getElementById("value");
 
-const addy = clickAdd.firstElementChild;
+// const addy = clickAdd.firstElementChild;
 
-const decrease = clickAdd.lastElementChild;
+// const decrease = clickAdd.lastElementChild;
+
+const addy = document.querySelector('button[data-action="decrement"]')
+const decrease = document.querySelector('button[data-action="increment"]')
 
 addy.addEventListener("click", onClick);
 function onClick() {
